fcd84dad15ca1716ae0f2e59a3aaa391d38a58c8
SamarAhmeed <samarabdo1811@gmail.com>
2019-02-05 23:14:54 +0200

    Category Done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_05_193005) do
+ActiveRecord::Schema.define(version: 2019_02_05_210909) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "cat_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
